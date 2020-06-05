dc0bb563aafffc19a543a2873e2b77f52dea0688
Alejandro Noblecilla <ale_14_nb@hotmail.com>
2020-06-05 15:06:35 -0500

    listaproductoscontroller

--- src/main/java/com/sw2/exparcialg3/controller/ListaProductosController.java
+++ src/main/java/com/sw2/exparcialg3/controller/ListaProductosController.java
@@ -1,5 +1,6 @@
 package com.sw2.exparcialg3.controller;
 
+import com.sw2.exparcialg3.entity.Producto;
 import com.sw2.exparcialg3.repository.PedidoRepository;
 import com.sw2.exparcialg3.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ListaProductos")
@@ -19,8 +23,20 @@ public class ListaProductosController {
 
     @GetMapping(value = {"", "/"})
     public String Lista(Model model){
-        model.addAttribute("productos", productoRepository.findAll());
-        return "";
+
+        model.addAttribute("productos", productoRepository.findByStockIsGreaterThan(0));
+        return "producto/listaProducto";
+    }
+
+    @GetMapping("/VerMas")
+    public String VerMas(@RequestParam("cod") String cod, Model model){
+
+        Optional<Producto> optProd = productoRepository.findById(cod);
+        if(optProd.isPresent()){
+            Producto prod = optProd.get();
+            model.addAttribute("producto", prod);
+        }
+        return "producto/verMas";
     }
 
 }
