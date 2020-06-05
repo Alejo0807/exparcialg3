0fbd425414e573b6ca136f494bbd347318752926
Alejandro Noblecilla <ale_14_nb@hotmail.com>
2020-06-05 17:06:42 -0500

    controladores

--- src/main/java/com/sw2/exparcialg3/controller/ListaProductosController.java
+++ src/main/java/com/sw2/exparcialg3/controller/ListaProductosController.java
@@ -1,12 +1,19 @@
 package com.sw2.exparcialg3.controller;
 
+import com.sw2.exparcialg3.constantes.PedProdId;
+import com.sw2.exparcialg3.entity.Pedido;
+import com.sw2.exparcialg3.entity.PedidoHasProducto;
 import com.sw2.exparcialg3.entity.Producto;
+import com.sw2.exparcialg3.entity.Usuario;
+import com.sw2.exparcialg3.repository.PedidoHasProductoRepository;
 import com.sw2.exparcialg3.repository.PedidoRepository;
 import com.sw2.exparcialg3.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,6 +27,9 @@ public class ListaProductosController {
     ProductoRepository productoRepository;
     @Autowired
     PedidoRepository pedidoRepository;
+    @Autowired
+    PedidoHasProductoRepository pedidoHasProductoRepository;
+
 
     @GetMapping(value = {"", "/"})
     public String Lista(Model model){
@@ -36,7 +46,29 @@ public class ListaProductosController {
             Producto prod = optProd.get();
             model.addAttribute("producto", prod);
         }
-        return "producto/verMas";
+        return "producto/verProducto";
+    }
+
+    @PostMapping("/AgregarAlCarrito")
+    public String AgregarCarrito(@RequestParam("cod") String cod){
+
+        Usuario usuario = new Usuario();
+        Optional<Producto> optProd = productoRepository.findById(cod);
+
+        Optional<Pedido> optPed = pedidoRepository.findById("carrito_"+ usuario.getDni());
+
+        if(optProd.isPresent()){
+            Producto prod = optProd.get();
+            if(optPed.isPresent()){
+                Pedido ped = optPed.get();
+                PedidoHasProducto php = new PedidoHasProducto(new PedProdId(ped, prod), 1);
+                //guardar de otra manera
+                pedidoHasProductoRepository.save(php);
+            }
+
+        }
+
+        return "redirect:/ListaProductos";
     }
 
 }
--- src/main/java/com/sw2/exparcialg3/repository/PedidoRepository.java
+++ src/main/java/com/sw2/exparcialg3/repository/PedidoRepository.java
@@ -4,6 +4,10 @@ import com.sw2.exparcialg3.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, String> {
+
+    Optional<Pedido> findByComprado(int value);
 }
