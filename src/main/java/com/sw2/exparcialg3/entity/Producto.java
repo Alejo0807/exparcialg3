57019b8f8bc202daed448db53bdba24deb30574a
gmn14 <meza.gustavo@pucp.edu.pe>
2020-06-06 00:48:11 -0500

    updatesss

--- src/main/java/com/sw2/exparcialg3/entity/Producto.java
+++ src/main/java/com/sw2/exparcialg3/entity/Producto.java
@@ -1,9 +1,15 @@
 package com.sw2.exparcialg3.entity;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -11,16 +17,22 @@ import java.io.Serializable;
 public class Producto implements Serializable {
 
     @Id
+    @NotBlank(message = "no puede estar vacío")
     @Column(name = "codigo")
     private String codigo;
+    @Size(max = 40, message = "Debe tener 40 caracteres como máximo")
+    @NotBlank(message = "no puede estar vacío")
     @Column(name = "nombre",nullable = false)
     private String nombre;
+    @NotBlank
+    @Size(max = 255, message = "Tiene que ser 255 caracteres como máximo")
     @Column(name = "descripcion",nullable = false)
     private String descripcion;
     @Column(name = "precio",nullable = false)
     private Float precio;
     @Column(name = "foto",nullable = false)
     private String foto;
+    @Positive
     @Column(name = "stock",nullable = false)
     private int stock;
 
